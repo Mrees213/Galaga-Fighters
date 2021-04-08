95c6e13fcf9bac2bb03ca647d1eacca528131e9a
Timothy Price <theCatintheFedora1367@gmail.com>
2021-04-08 15:39:31 -0500

    Initial Commit

---
+++ Galaga Fighters macOS/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  Galaga Fighters macOS
+//
+//  Created by Student on 4/8/21.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
